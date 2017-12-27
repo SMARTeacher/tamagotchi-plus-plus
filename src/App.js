bc60ca791f3e0434fd9098f3785d4584df7b8611
Joe Staller <joestaller23@gmail.com>
2017-12-27 09:45:05 -0500

    A fish has been rendered!

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import fish from './fish.png';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    componentDidMount() {
+        this.updateCanvas();
+    }
+
+    updateCanvas = () => {
+        const context = this.refs.canvas.getContext('2d');
+        const fish = this.refs.fish;
+        fish.onload = () => {
+            context.drawImage(fish, 20, 20, 40, 40);
+        };
+    };
+
+    render() {
+        const square = 500;
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1 className="App-title">Welcome to THE FISHTANK</h1>
+                </header>
+                <canvas
+                    ref="canvas"
+                    height={square}
+                    width={square}
+                    style={{ border: '1px solid #000' }}
+                />
+                <img src={fish} ref="fish" style={{ display: 'none' }} width={128} height={128} />
+            </div>
+        );
+    }
 }
 
 export default App;
