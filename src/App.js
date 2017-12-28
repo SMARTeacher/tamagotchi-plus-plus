8ff8763dd4312866cf9350ee5826471986d78967
Karl Merten <karl.merten@prodigygame.com>
2017-12-28 14:44:30 -0500

    Added rest behaviour

--- src/App.js
+++ src/App.js
@@ -415,6 +415,22 @@ class FleeBehaviour extends MoveTowardBehaviour {
     }
 }
 
+class RestBehaviour {
+    constructor(minDuration, maxDuration) {
+        this.restDuration = minDuration + (Math.random() * (maxDuration - minDuration));
+        this.running = true
+    }
+
+    update() {
+        if (this.running) {
+            this.restDuration -= (frameRate * 0.001);
+            if (this.restDuration < 0) {
+                this.running = false;
+            }
+        }
+    }
+}
+
 class App extends Component {
     state = {
         interval: null
