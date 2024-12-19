920c22e7257f234f067ac8d00fcb5bcb2b44ccfd
Joe Staller <joestaller23@gmail.com>
2024-12-19 15:10:48 -0500

    WIP

--- src/constants/fishConstants.js
+++ src/constants/fishConstants.js
@@ -1,38 +1,34 @@
-import {
-    redFish,
-    blueFish,
-    greenFish,
-    orangeFish,
-} from '../assets';
+import { redFish, blueFish, greenFish, orangeFish } from "../assets";
 
 const fishes = [
-  { src: redFish, type: 'redFish' },
-  { src: blueFish, type: 'blueFish' },
-  { src: greenFish, type: 'greenFish' },
-  { src: orangeFish, type: 'orangeFish' },
+  { src: redFish, type: "redFish" },
+  { src: blueFish, type: "blueFish" },
+  { src: greenFish, type: "greenFish" },
+  { src: orangeFish, type: "orangeFish" },
 ];
 
-const fishTypes = fishes.map(fish => fish.type);
+const fishTypes = fishes.map((fish) => fish.type);
 
 const fishPersonalities = {
   redFish: [
-    { type: 'cheese', likes: true },
-    { type: 'redFish', likes: true }
-    //    { type: 'greenFish', likes: false },
-    //    { type: 'blueFish', likes: true }
+    { type: "cheese", desirePoints: 40 },
+    { type: "redFish", desirePoints: 10 },
+    { type: "greenFish", desirePoints: 10 },
+    { type: "blueFish", desirePoints: 10 },
   ],
   blueFish: [
-    { type: 'cheese', likes: false },
-    { type: 'blueFish', likes: true }
+    { type: "cheese", desirePoints: 20 },
+    { type: "blueFish", desirePoints: 20 },
+    { type: "rest", desirePoints: 50 },
   ],
   greenFish: [
-    { type: 'cheese', likes: true },
-    { type: 'orangeFish', likes: true },
-    { type: 'greenFish', likes: true }
+    { type: "cheese", desirePoints: 100 },
+    { type: "orangeFish", desirePoints: 30 },
+    { type: "greenFish", desirePoints: 60 },
   ],
   orangeFish: [
-    { type: 'cheese', likes: true },
-    { type: 'orangeFish', likes: true }
+    { type: "cheese", desirePoints: 30 },
+    { type: "orangeFish", desirePoints: 70 },
   ],
 };
 
--- src/operations/addFish.js
+++ src/operations/addFish.js
@@ -1,29 +1,30 @@
-import { fishTypes, fishPersonalities } from '../constants/fishConstants';
+import { fishTypes, fishPersonalities } from "../constants/fishConstants";
 
 import {
-    fishTankSize,
-    boredomRadius,
-    fishWaitingPeriod,
-    fishThreshold,
-    theDannyConstant,
-} from '../config';
+  fishTankSize,
+  boredomRadius,
+  fishWaitingPeriod,
+  fishThreshold,
+  theDannyConstant,
+} from "../config";
 import {
-    coinFlip,
-    sortDesiresByDistance,
-    getSmallDistance,
-    clamp,
-} from './helpers';
+  coinFlip,
+  sortDesiresByDistance,
+  getSmallDistance,
+  clamp,
+} from "./helpers";
 
 let id = 1;
 
-const addFish = options => {
+const addFish = (options) => {
   const { school, globalRefs, fishType: type, cheeses } = options;
   if (school.length >= theDannyConstant) return;
   if (globalRefs === undefined) return;
   //all types
-  const fishType = type || fishTypes[Math.floor(Math.random() * (fishTypes.length - 1))];
+  const fishType =
+    type || fishTypes[Math.floor(Math.random() * (fishTypes.length - 1))];
   const speedModifier = 2;
-  console.log('New Fish!');
+  console.log("New Fish!");
   const newFish = {
     id: `${fishType}${id}`,
     fish: globalRefs[fishType],
@@ -37,34 +38,49 @@ const addFish = options => {
     restingPeriod: 0,
     size: 40,
     speedModifier,
-    isBored: function() {
+    isBored: function () {
       const xDiff = Math.abs(this.x - this.desireX);
       const yDiff = Math.abs(this.y - this.desireY);
 
-      return this.restingPeriod === 0 && xDiff <= this.speedModifier && yDiff <= this.speedModifier;
+      return (
+        this.restingPeriod === 0 &&
+        xDiff <= this.speedModifier &&
+        yDiff <= this.speedModifier
+      );
     },
-    fidget: function() {
-      this.desireX = coinFlip() ? this.x + boredomRadius : this.x - boredomRadius;
-      this.desireY = coinFlip() ? this.y + boredomRadius : this.y - boredomRadius;
+    fidget: function () {
+      this.desireX = coinFlip()
+        ? this.x + boredomRadius
+        : this.x - boredomRadius;
+      this.desireY = coinFlip()
+        ? this.y + boredomRadius
+        : this.y - boredomRadius;
       this.restingPeriod = fishWaitingPeriod;
     },
-    moveToDesire: function() {
+    moveToDesire: function () {
       // X movement
       const moveX = this.desireX - this.x;
       if (Math.abs(moveX) > this.speedModifier) {
-        moveX > 0 ? (this.x += this.speedModifier) : (this.x -= this.speedModifier);
+        moveX > 0
+          ? (this.x += this.speedModifier)
+          : (this.x -= this.speedModifier);
       }
 
       // Y movement
       const moveY = this.desireY - this.y;
       if (Math.abs(moveY) > this.speedModifier) {
-        moveY > 0 ? (this.y += this.speedModifier) : (this.y -= this.speedModifier);
+        moveY > 0
+          ? (this.y += this.speedModifier)
+          : (this.y -= this.speedModifier);
       }
     },
-    setNewDesire: function(canFidget = true) {
+    setNewDesire: function (canFidget = true) {
       //reset speed
       this.speedModifier = 1;
-      const closeFish = sortDesiresByDistance(this, school, cheeses).slice(0, fishThreshold);
+      const closeFish = sortDesiresByDistance(this, school, cheeses).slice(
+        0,
+        fishThreshold
+      );
 
       if (canFidget && coinFlip()) {
         this.fidget();
@@ -72,21 +88,42 @@ const addFish = options => {
         let desireObject;
         let desireMode;
         let index = 0;
-        while (!desireObject && index < this.personality.length) {
-          const currentFishCheck = this.personality[index];
-          const foundDesire = closeFish.find(fish => fish.type === currentFishCheck.type);
-          if (foundDesire) {
-            desireObject = foundDesire;
-            desireMode = currentFishCheck.likes;
+        let desireObjectType;
+
+        // const totalDesirePoints = this.personality.reduce(
+        //   (prev, curr) => prev + curr.desirePoints,
+        //   0
+        // );
+        // const desireRoll = Math.ceil(Math.random() * totalDesirePoints); // 47
+        // let sum = 0;
+        // for (let personalityRow of this.personality) {
+        //   sum += personalityRow.desirePoints;
+        //   if (sum - desireRoll <= 0) {
+        //     desireObjectType = personalityRow.type;
+        //     break;
+        //   }
+        // }
+
+        console.log(desireObjectType)
+
+        if (!desireObject)
+          while (!desireObject && index < this.personality.length) {
+            const currentFishCheck = this.personality[index];
+            const foundDesire = closeFish.find(
+              (fish) => fish.type === currentFishCheck.type
+            );
+            if (foundDesire) {
+              desireObject = foundDesire;
+              desireMode = currentFishCheck.likes;
+            }
+            index++;
           }
-          index++;
-        }
 
         if (desireObject) {
           // likes object
           if (desireMode === true) {
             this.currentDesireType = desireObject.type;
-            
+
             this.desireX = coinFlip()
               ? desireObject.x + getSmallDistance()
               : desireObject.x - getSmallDistance();
@@ -101,14 +138,22 @@ const addFish = options => {
               this.desireX = Math.floor(Math.random() * (fishTankSize / 4));
               this.desireY = Math.floor(Math.random() * (fishTankSize / 4));
             } else if (desireObject.x <= 250 && desireObject.y > 250) {
-              this.desireX = Math.floor(Math.random() * (fishTankSize / 4)) + fishTankSize / 2;
+              this.desireX =
+                Math.floor(Math.random() * (fishTankSize / 4)) +
+                fishTankSize / 2;
               this.desireY = Math.floor(Math.random() * (fishTankSize / 4));
             } else if (desireObject.x > 250 && desireObject.y <= 250) {
               this.desireX = Math.floor(Math.random() * (fishTankSize / 4));
-              this.desireY = Math.floor(Math.random() * (fishTankSize / 4)) + fishTankSize / 2;
+              this.desireY =
+                Math.floor(Math.random() * (fishTankSize / 4)) +
+                fishTankSize / 2;
             } else if (desireObject.x <= 250 && desireObject.y <= 250) {
-              this.desireX = Math.floor(Math.random() * (fishTankSize / 4)) + fishTankSize / 2;
-              this.desireY = Math.floor(Math.random() * (fishTankSize / 4)) + fishTankSize / 2;
+              this.desireX =
+                Math.floor(Math.random() * (fishTankSize / 4)) +
+                fishTankSize / 2;
+              this.desireY =
+                Math.floor(Math.random() * (fishTankSize / 4)) +
+                fishTankSize / 2;
             }
           }
         } else {
@@ -118,7 +163,7 @@ const addFish = options => {
 
       this.desireX = clamp(this.desireX, 0, fishTankSize - this.size);
       this.desireY = clamp(this.desireY, 0, fishTankSize - this.size);
-    }
+    },
   };
   id++;
   newFish.setNewDesire(false);
