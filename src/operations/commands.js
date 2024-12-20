1cc6ab1f659440a151b09053ee261187574af439
Jonathan 'Nathan' Pitre <jonathan.pitre@prodigygame.com>
2024-12-20 14:29:34 -0500

    commands

---
+++ src/operations/commands.js
@@ -0,0 +1,42 @@
+const processScript = (firstNode) => {
+    let node = firstNode;
+    while (node) {
+        node.perform();
+        // TODO: Add a pause
+        if (node.getNext()) {
+            node = node.getNext();
+        }
+    }
+}
+
+const newActionNode = (actionFunction) => {
+    return {
+        type: "action",
+        perform: () => actionFunction(),
+        getNext: null
+    }
+};
+
+const newConditionalNode = (conditionalFunction, ifTrueNode, ifFalseNode) => {
+    return {
+        type: "if",
+        perform: () => {
+            if (conditionalFunction()) {
+                this.getNext = () => ifTrueNode;
+            }
+            else {
+                this.getNext = () => ifFalseNode;
+            }
+        },
+        getNext: null,
+    }
+}
+
+const newNoOpCommand = newActionCommand(() => {});
+
+export {
+    processScript,
+    newActionNode,
+    newConditionalNode,
+    newNoOpCommand,
+};
